6ea1ba5eee6be0f1f5686f623de24a0cf5505b3b
pedromeireles23 <pemeirels@gmail.com>
2024-05-26 17:23:30 -0300

    outros-commits

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// outrocommit
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
 const restartBtn = document.querySelector("#restartBtn");
