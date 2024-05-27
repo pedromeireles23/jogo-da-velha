bf2f47fd9ceb428b394aa92367d1ed665683788a
pedromeireles23 <pemeirels@gmail.com>
2024-05-27 02:42:17 -0300

    matriz multi-dimensional

--- matrizes/index.js
+++ matrizes/index.js
@@ -1,10 +1,9 @@
-// 2D array = multi-dimensional array that stores a matrix
-//                    of data in rows and columns.
-//                    Useful for games, spreadsheets, or representing images 
+
 
 const matrix = [ [1, 2, 3], 
                  [4, 5, 6], 
                  [7, 8, 9]];
+//  Este trecho cria uma matriz bidimensional chamada matrix com 3 linhas e 3 colunas. Inicialmente, a matriz é preenchida com números de 1 a 9
 
 matrix[0][0] = 'X';
 matrix[0][1] = 'O';
@@ -17,8 +16,15 @@ matrix[1][2] = 'O';
 matrix[2][0] = 'X';
 matrix[2][1] = 'O';
 matrix[2][2] = 'X';
-
+// Esses comandos alteram os elementos da matriz original, substituindo os números por caracteres 'X' e 'O'. 
 for(let row of matrix){
     const rowString = row.join(' ');
     console.log(rowString);
+    // Este loop for itera sobre cada linha da matriz.
+    // Para cada linha (row), os elementos da linha são unidos em uma string única, separados por espaços, usando o método join(' ').
+    // Em seguida, essa string é impressa no console usando console.log.
 }
+
+// Inicializa uma matriz 3x3 com números de 1 a 9.
+// Modifica todos os elementos da matriz para formar um padrão com 'X' e 'O'.
+// Imprime a matriz resultante no console, linha por linha, onde cada linha é formatada como uma string com os elementos separados por espaços.
